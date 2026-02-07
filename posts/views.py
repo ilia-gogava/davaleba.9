27e3dc618f0f2c4d48f864075304b8c4f52af859
iliaG <ilia.gogava141@gmail.com>
2026-02-07 23:13:35 +0400

    new

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from .models import Post
 
-# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    success_url = reverse_lazy('post_list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    success_url = reverse_lazy('post_list')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
